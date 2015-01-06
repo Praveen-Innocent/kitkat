6d280bab293b19689becc6a70f65971fe3062988
PC <pravin.innocent@gmail.com>
2015-01-06 09:10:32 +0530

    adding basic mvc framework files

---
+++ application/views/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    
+    <title>QuickSilver</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+    
+    <link rel="stylesheet" href="<?php echo Config::$URL; ?>static/css/bootstrap.css" type="text/css" media="screen" />
+    <link rel="stylesheet" href="<?php echo Config::$URL; ?>static/css/style.css" type="text/css" media="screen" />
+</head>
+<body>
